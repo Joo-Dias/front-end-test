8c2e2e3e02c88cfd0fb05247c7c71d03615cb1e8
João Dias <joaovitorelf@gmail.com>
2022-03-09 12:18:51 -0300

    Finalizando o projeto

--- projeto-api-github/src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import './Button.scss'
-
-export function Button({icon}) {
-    return (
-        <button class="button is-link">{icon}</button>
-    )
-}
--- projeto-api-github/src/components/Input/Input.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export function Input({placeholder}) {
-    return(
-        <input class="input" type="text" placeholder={placeholder} />
-    )
-}
